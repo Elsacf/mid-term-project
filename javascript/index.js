fed09e4f9e12bb6cbb04b94605c3193accd0cce3
Elsa Castillo Franco <elsacastillofranco@Air-de-Elsa.home>
2022-08-10 01:03:48 +0200

    Day ten

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,14 @@
+const tch = document.querySelector("#tch")
 
+tch.addEventListener("click", function inTouch(){
+    window.location = "contact.html"
+}); 
+
+let emailOne = document.querySelector("#email-one")
+function suscription (){
+    if (!emailOne) {
+        alert("please, write your email");
+    } else if (typeof emailOne !== Text)
+        alert("is not a correct form");
+        else if ()
+}
